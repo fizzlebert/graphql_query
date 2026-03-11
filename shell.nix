f14a9a7b52271ed46b58075944cc19c09e67a779
Daniel O'Connell <dan.ben.oconnell@gmail.com>
2026-03-11 22:19:57 +1100

    chore: nix files

---
+++ shell.nix
@@ -0,0 +1,14 @@
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-25.11";
+  pkgs = import nixpkgs { config = {}; overlays = []; };
+in
+
+pkgs.mkShellNoCC {
+  packages = with pkgs; [
+    elixir_1_19
+    erlang_28
+    elixir-ls
+    rustup
+    rust-analyzer
+  ];
+}
